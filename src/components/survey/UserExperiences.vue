acd87865317680be7e339a229a7c31ac9a9d5e06
Nikodem <nikodem@nicode.pl>
2022-07-12 17:48:02 +0200

    loading added

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,10 +28,12 @@ export default {
   data(){
     return {
       results:[],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences(){
+      this.isLoading = true;
       fetch('https://vue-http-demo-c974f-default-rtdb.firebaseio.com/surveys.json')
       .then((response) => {
           if(response.ok){
@@ -39,6 +42,7 @@ export default {
         }
       )
       .then((data) => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({
